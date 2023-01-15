b1539349f32fbfbdd121e2c90d5964ea172c0a8b
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-01-14 20:50:07 -0800

    fixed error

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,10 @@ searchBar.addEventListener('click', (event) => {
   // Get the search query
   const query = event.target.value.toUpperCase();
   // Replace the hardcoded path with the input value
-  var requestUrl1 = `https://api.coinpaprika.com/v1/coins/${query}/ ` ;
-  var requestUrl2 = `https://api.coinpaprika.com/v1/coins/${query}/ohlcv/latest`;
+
+  var requestUrl1 = `https://api.coinpaprika.com/v1/coins/${query}/ohlcv/latest`;
+
   fetch(requestUrl1)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      console.log(data)
-      console.log(data.name);
-    });
-  fetch(requestUrl2)
     .then(function(response) {
       return response.json();
     })
