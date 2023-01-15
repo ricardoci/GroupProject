540619bcb1a03a20e5d8b974c99e36ff4648d1db
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-01-14 20:47:21 -0800

    gettin each currency price and data went entered into search-bar

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,27 @@
 const searchBar = document.querySelector('#search-bar');
 
-
-searchBar.addEventListener('input', (event) => {
+searchBar.addEventListener('click', (event) => {
   // Get the search query
   const query = event.target.value.toUpperCase();
-
-  function getApi() {
-    var requestUrl1 = `https://api.coinpaprika.com/v1/coins/btc-bitcoin`;
-    var requestUrl2 = `https://api.coinpaprika.com/v1/coins/btc-bitcoin/ohlcv/latest`;
-    fetch(requestUrl1)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data)
-        console.log(data.name, );
-        
-      });
-    fetch(requestUrl2)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-       console.log(data)
-       console.log( data[0].open, data[0].time_open );
-        
-        
-      });
-  }
-  getApi();
+  // Replace the hardcoded path with the input value
+  var requestUrl1 = `https://api.coinpaprika.com/v1/coins/${query}/ ` ;
+  var requestUrl2 = `https://api.coinpaprika.com/v1/coins/${query}/ohlcv/latest`;
+  fetch(requestUrl1)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data)
+      console.log(data.name);
+    });
+  fetch(requestUrl2)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+     console.log(data)
+     console.log( data[0].open, data[0].time_open );
+    });
 });
 
 
