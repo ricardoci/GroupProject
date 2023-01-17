3bb9e9922be8b091e00c45de84f65c1da064f5b8
Ricardo Cisneros <ricardocisneros197@gmail.com>
2023-01-16 22:02:00 -0800

    button input on submition fixed

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,32 @@
+
 const searchBar = document.querySelector('#search-bar');
+const searchButton = document.querySelector('#search-button');
 
-searchBar.addEventListener('click', (event) => {
-  // Get the search query
-  const query = event.target.value.toUpperCase();
-  // Replace the hardcoded path with the input value
-
-  var requestUrl1 = `https://api.coinpaprika.com/v1/coins/${query}/ohlcv/latest`;
-
-  fetch(requestUrl1)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-     console.log(data)
-     console.log( data[0].open, data[0].time_open );
-    });
-});
+function api(event) {
+  event.preventDefault()
+  if (searchBar.value) {
+    const query = searchBar.value.toUpperCase();
+    var requestUrl1 = `https://api.coinpaprika.com/v1/coins/${query}/ohlcv/latest`;
+    
+    searchBar.value = '';
 
 
+    fetch(requestUrl1)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        
+        console.log(data[0].open, data[0].time_open,);
+      });
+      
+  }
+ 
+   else {
+    console('Please enter a crypto name');
+  }
+ 
+};
 
-
-
-
+searchButton.addEventListener('click', api);
 
